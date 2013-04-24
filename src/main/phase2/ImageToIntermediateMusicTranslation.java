c8d036fb3b852a9242195a5669b0de1199206019
cimbriano <Christopher.Imbriano@gmail.com>
2013-04-23 23:49:16 -0400

    Working on structure, but unsure of Map Input

--- src/main/phase2/ImageToIntermediateMusicTranslation.java
+++ src/main/phase2/ImageToIntermediateMusicTranslation.java
@@ -1,13 +1,27 @@
 package phase2;
 
 import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
 import org.apache.log4j.Logger;
 
+import edu.umd.cloud9.io.pair.PairOfInts;
+
 public class ImageToIntermediateMusicTranslation extends Configured implements Tool {
   private static final Logger LOG = Logger.getLogger(ImageToIntermediateMusicTranslation.class);
 
+  //Mapper: emits ...
+  private static class PixelToToneMapper extends Mapper<LongWritable, Text, IntWritable, PairOfInts>{
+    private static final IntWritable IMAGE_REGION = new IntWritable();
+    private static final PairOfInts NOTE = new PairOfInts();
+
+
+//    public void map(LongWritable key, Text value ){}
+  }
   
   public ImageToIntermediateMusicTranslation() {}
   
