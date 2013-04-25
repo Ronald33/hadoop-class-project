53cc15c0d69a363c4fc34c0e9481721701615084
cimbriano <Christopher.Imbriano@gmail.com>
2013-04-24 21:27:58 -0400

    Adding static variables for midi note values and Octave Range. Changed method name to keep terminology consistent

--- src/main/phase2/UsingColorToConvertRgbIntoMusicNotes.java
+++ src/main/phase2/UsingColorToConvertRgbIntoMusicNotes.java
@@ -1,13 +1,29 @@
 package phase2;
 
 import java.awt.Color;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Random;
 
 
 public class UsingColorToConvertRgbIntoMusicNotes {
-  
-  public static int LOWEST_NOTE = 12;
 
+  public static final int LOWEST_NOTE = 12;
+  public static final int OCTAVE_RANGE = 9;
+  public static final int C = 0;
+  public static final int C_SHARP = 1;
+  public static final int D = 2;
+  public static final int D_SHARP = 3;
+  public static final int E = 4;
+  public static final int F = 5;
+  public static final int F_SHARP = 6;
+  public static final int G = 7;
+  public static final int G_SHARP = 8;
+  public static final int A = 9;
+  public static final int A_SHARP = 10;
+  public static final int B = 11;
+  
   /**
    * @param args
    */
@@ -68,17 +84,19 @@ public class UsingColorToConvertRgbIntoMusicNotes {
    * returned tone should be between 0 and 11 inclusive. 
    * @return
    */
-  public static int hueFloatToToneInt(float hueFloat){
-    
+  public static int hueFloatToNoteInt(float hueFloat){
+
     System.out.println("hueFLoat: " + hueFloat);
 
     int hueAngle = (int) Math.floor(hueFloat * 360);
     System.out.println("hueAngle: " + hueAngle);
+
+    int region = hueAngle / 30;
+    int tone = regionToMidiNote[region] + LOWEST_NOTE;
     
-    int tone = hueAngle / 30;
     
-    
-    return tone + LOWEST_NOTE;
+    return tone;
+
   }
 
 }
