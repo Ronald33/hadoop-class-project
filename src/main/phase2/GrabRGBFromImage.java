ce336fbe4478f0298d0dad137b7fae7ab9d75fcb
acstrick <acstrick@umd.edu>
2013-04-25 20:21:50 -0400

    Added image rescaling code and sample data

--- src/main/phase2/GrabRGBFromImage.java
+++ src/main/phase2/GrabRGBFromImage.java
@@ -1,6 +1,7 @@
 package phase2;
 
 import java.awt.Color;
+import java.awt.Graphics2D;
 import java.awt.image.*;
 import java.io.File;
 import java.io.IOException;
@@ -11,7 +12,7 @@ public class GrabRGBFromImage {
   public static void main(String[] args) {
     BufferedImage img = null;
     try {
-      img = ImageIO.read(new File("magenta.jpg"));
+      img = ImageIO.read(new File("data/one.jpg"));
     } catch (IOException e) {
     }
     
@@ -25,6 +26,17 @@ public class GrabRGBFromImage {
     System.out.println("red: "+red);
     System.out.println("green: "+green);
     System.out.println("blue: "+blue);
+    
+    // Resize image to 3X3 pixels
+    BufferedImage scaledImg = new BufferedImage(3,3,BufferedImage.TYPE_INT_RGB);
+    Graphics2D g = scaledImg.createGraphics();
+    g.drawImage(img,0,0,3,3,null);
+    g.dispose();
+    File outputfile = new File("data/oneScaled.jpg");
+    try {
+      ImageIO.write(scaledImg,  "jpg", outputfile);
+    } catch (IOException e) {
+    }
   }
 
 }
