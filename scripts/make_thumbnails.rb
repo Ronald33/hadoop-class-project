dd77c24a12a1261f1e38e2e7da1333665f9132ba
cimbriano <Christopher.Imbriano@gmail.com>
2013-04-25 20:00:06 -0400

    Scripts to create thumbnails from image, and another to blow up the thumbnail

---
+++ scripts/make_thumbnails.rb
@@ -0,0 +1,12 @@
+require 'rmagick'
+
+img = Magick::Image::read("images/road.jpg")[0]
+
+thumb = img.thumbnail(3, 3)
+
+thumb.write("images/road_thumb.jpg")
+
+# Dir.glob("*.jpg") do |fname|
+#    img = Magick::Image.read(fname)[0]
+#    img.thumbnail(0.10).write("#{fname}-thumb.jpg")
+# end   
