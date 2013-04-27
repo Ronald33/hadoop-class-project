2ca6be29301ba7b1b5a9bba092f007155a08d276
cimbriano <Christopher.Imbriano@gmail.com>
2013-04-26 22:34:31 -0400

    Initial commit of Color2Music

---
+++ src/main/phase2reverse/Music2Color.java
@@ -0,0 +1,18 @@
+package phase2reverse;
+
+public class Music2Color {
+
+  /**
+   * @param args
+   */
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+
+  }
+  
+  public static float[] getHSBFromNote(int tone, int velocity){
+    
+    return null;
+  }
+
+}
