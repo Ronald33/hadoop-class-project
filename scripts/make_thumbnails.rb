7b571e7470d577d1732c9cffbe90e33d62c63437
cimbriano <Christopher.Imbriano@gmail.com>
2013-04-27 20:19:26 -0400

    Script takes a path to a directory as a parameter and makes thumbs of all jpgs in that directory

--- scripts/make_thumbnails.rb
+++ scripts/make_thumbnails.rb
@@ -1,12 +1,28 @@
 require 'rmagick'
 
-img = Magick::Image::read("images/road.jpg")[0]
+directory = ARGV.shift
 
-thumb = img.thumbnail(3, 3)
+if(Dir.exists? directory)
+	# Make directory name string for thumbs
+	thumb_directory = "#{directory}-thumbs"
 
-thumb.write("images/road_thumb.jpg")
+	# Actually make the directory
+	Dir.mkdir(thumb_directory) unless Dir.exists?(thumb_directory)
 
-# Dir.glob("*.jpg") do |fname|
-#    img = Magick::Image.read(fname)[0]
-#    img.thumbnail(3,3).write("#{fname}-thumb.jpg")
-# end   
+	Dir.glob("#{directory}/*.jpg") do |fname|
+		puts fname
+		filename_parts = fname.split("/")
+		path = filename_parts[0, filename_parts.length - 1]
+		path[path.length - 1] = path.last + "-thumbs"
+
+		thumb_fname = "thumb-#{filename_parts.last}"
+
+
+		img = Magick::Image.read(fname)[0]
+		thumb = img.thumbnail(3,3)
+		thumb.write(path.join("/") + "/#{thumb_fname}")
+	end
+
+else
+	puts "Usage: 'ruby scripts/make_thumbnails.rb <directory>'"
+end
