e32b245de878988c01920f2b08d850c2341f2c4d
acstrick <acstrick@umd.edu>
2013-04-27 23:36:59 -0400

    Added BytesWritable test

--- src/main/phase2/GrabRGBFromImage.java
+++ src/main/phase2/GrabRGBFromImage.java
@@ -3,18 +3,41 @@ package phase2;
 import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.image.*;
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
 import java.io.File;
 import java.io.IOException;
+import java.io.ObjectOutput;
+import java.io.ObjectOutputStream;
+
 import javax.imageio.ImageIO;
+
+import org.apache.hadoop.io.BytesWritable;
 
 public class GrabRGBFromImage {
 
-  public static void main(String[] args) {
+  public static void main(String[] args) throws IOException {
     BufferedImage img = null;
+    ByteArrayOutputStream bOut = new ByteArrayOutputStream();
+    ObjectOutput out = new ObjectOutputStream(bOut);
+    
+    // Read an image from file
     try {
       img = ImageIO.read(new File("data/one.jpg"));
     } catch (IOException e) {
     }
+    
+    // Get the RGB integer for each pixel in 3X3
+    for(int i=1; i<4; i++) {
+      for(int j=1; j<4; j++) {
+        out.writeInt(img.getRGB(i, j));
+      }
+    }
+    out.close();
+    
+    // Make a BytesWritable and with the output stream data
+    BytesWritable BW = new BytesWritable(bOut.toByteArray());
+    bOut.close();
     
     
     Color c = new Color(img.getRGB(1,1));
@@ -27,7 +50,7 @@ public class GrabRGBFromImage {
     System.out.println("green: "+green);
     System.out.println("blue: "+blue);
     
-    // Resize image to 3X3 pixels
+    // Resize image
     BufferedImage scaledImg = new BufferedImage(3,3,BufferedImage.TYPE_INT_RGB);
     Graphics2D g = scaledImg.createGraphics();
     g.drawImage(img,0,0,3,3,null);
